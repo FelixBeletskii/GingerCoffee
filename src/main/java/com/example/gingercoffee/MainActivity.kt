aa4962176e98a5161db8e24fa78f9088088ef088
felixbeletskii <felixbeletskii@bebs.app>
2024-07-11 10:01:40 +0600

    main changes. need too make branches

--- src/main/java/com/example/gingercoffee/MainActivity.kt
+++ src/main/java/com/example/gingercoffee/MainActivity.kt
@@ -1,18 +1,20 @@
 package com.example.gingercoffee
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.GravityCompat
 import com.example.gingercoffee.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    lateinit var bind: ActivityMainBinding
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        bind = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(bind.root)
-
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        with(binding){
+            ivMenu.setOnClickListener {
+                drawer.openDrawer(GravityCompat.START)
+            }
+        }
         }
     }
