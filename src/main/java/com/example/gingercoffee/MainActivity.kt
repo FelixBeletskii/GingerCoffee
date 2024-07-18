9c85c989129ebb7f6d4aff7f023ad200a8988ed3
felixbeletskii <felixbeletskii@bebs.app>
2024-07-18 11:07:07 +0600

    ресайклер готов. слушатель нажатий подготовлен

--- src/main/java/com/example/gingercoffee/MainActivity.kt
+++ src/main/java/com/example/gingercoffee/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.gingercoffee
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import androidx.fragment.app.Fragment
@@ -10,9 +11,9 @@ import com.example.gingercoffee.data.Drink
 import com.example.gingercoffee.data.model.DrinkAdapter
 import com.example.gingercoffee.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), DrinkAdapter.Listener {
 
-    private val adapter = DrinkAdapter()
+    private val adapter = DrinkAdapter(this)
 
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,7 +27,10 @@ class MainActivity : AppCompatActivity() {
             }
         }
         init()
+        openFrag(RecyclerViewFragment(),R.id.place_holder_main)
+
     }
+    
 
     private fun openFrag(f: Fragment, idHolder: Int) {
         supportFragmentManager.beginTransaction().replace(idHolder, f).commit()
@@ -35,16 +39,22 @@ class MainActivity : AppCompatActivity() {
 
     fun init() {
         val drinksList: Array<String> = resources.getStringArray(R.array.Drinks)
-        val priceList : IntArray = resources.getIntArray(R.array.price)
 
         binding.apply {
             recycler.layoutManager = LinearLayoutManager(this@MainActivity)
             recycler.adapter = adapter
+
             for (i in 0 until drinksList.size) {
-                val drink = Drink(drinksList[i], priceList[i])
+                val drink = Drink(drinksList[i])
+                drink.countDrink(drink.title)
                 adapter.addDrink(drink)
             }
 
         }
     }
-}
+
+    override fun onClick(drink: Drink) {
+          Toast.makeText(this,"Нажали на ${drink.title}",Toast.LENGTH_SHORT).show()
+
+    }
+    }
--- src/main/java/com/example/gingercoffee/data/model/DrinkAdapter.kt
+++ src/main/java/com/example/gingercoffee/data/model/DrinkAdapter.kt
@@ -3,20 +3,23 @@ package com.example.gingercoffee.data.model
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.example.gingercoffee.R
 import com.example.gingercoffee.data.Drink
 import com.example.gingercoffee.databinding.ItemDrinkBinding
 
-class DrinkAdapter: RecyclerView.Adapter<DrinkAdapter.DrinkHolder>() {
+class DrinkAdapter(val listener: Listener): RecyclerView.Adapter<DrinkAdapter.DrinkHolder>() {
     val drinkList = ArrayList<Drink>()
 
     class DrinkHolder(item: View) : RecyclerView.ViewHolder(item) {
         val binding = ItemDrinkBinding.bind(item)
-        fun bind(drink: Drink) {
+        fun bind(drink: Drink, listener: Listener) {
             with(binding) {
                 tvTitle.text = drink.title
                 tvPrice.text = drink.price.toString()
+          itemView.setOnClickListener {
+              listener.onClick(drink) }
             }
         }
     }
@@ -28,7 +31,7 @@ class DrinkAdapter: RecyclerView.Adapter<DrinkAdapter.DrinkHolder>() {
 
 
     override fun onBindViewHolder(holder: DrinkHolder, position: Int) {
-        holder.bind(drinkList[position])
+        holder.bind(drinkList[position], listener)
     }
 
 
@@ -39,6 +42,11 @@ class DrinkAdapter: RecyclerView.Adapter<DrinkAdapter.DrinkHolder>() {
     fun addDrink(drink:Drink){
         drinkList.add(drink)
         notifyDataSetChanged()
+    }
+
+    interface Listener{
+        fun onClick(drink:Drink)
+
     }
 
 }
